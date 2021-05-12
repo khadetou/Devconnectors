99104377e02337923fda51cbf8632cdef02c3793
khadetou <khadetou96@gmail.com>
2021-05-12 07:16:56 +0000

    put delete get profile

--- backend/routes/profiles.js
+++ backend/routes/profiles.js
@@ -79,4 +79,92 @@ check('skills', 'skills is required').not().isEmpty(),
     res.json(profile);
 }));
 
+
+//@route  GET/api/Profiles
+//@desc   Get all profiles
+//@access Public
+
+router.get('/', asyncHandler(async (req, res)=>{
+
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+
+}));
+
+
+//@route  GET/api/Profiles/user/:user_id
+//@desc   Get all profiles by user_id
+//@access Public
+
+router.get('/user/:user_id', asyncHandler(async (req, res)=>{
+
+    const profile = await Profile.findOne({user: req.params.user_id}).populate('user', ['name', 'avatar']);
+    
+    if(!profile) return res.status(400).json({msg: 'There is no profile for this user'});
+
+    res.json(profile);
+
+}));
+
+
+
+//@route  DELETE/api/Profile
+//@desc   Delete profile, user & post
+//@access Public
+
+router.delete('/', auth, asyncHandler(async (req, res)=>{
+
+    //Remove profile
+    await Profile.findOneAndRemove({user: req.user.id});
+
+    //Remove user
+    await User.findOneAndRemove({_id: req.user.id});
+
+
+    res.json({msg: 'User deleted'});
+
+}));
+
+//@route  PUT/api/Profile/experience
+//@desc   Add profile experience
+//@access Private
+
+router.put('/experience',[auth,
+     [
+     check('title','Title is required')
+     .not()
+     .isEmpty(),
+     check('company','Company is required')
+     .not()
+     .isEmpty(),
+     check('from','From date is required')
+     .not()
+     .isEmpty(),
+    ]], asyncHandler(async (req, res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+        const {title, company, location, from, to, current, description}= req.body;
+
+        const newExp ={
+            title,
+            company,
+            location,
+            from, 
+            to,
+            current,
+            description
+        }
+
+        const profile = await Profile.findOne({user: req.user.id});
+        profile.experience.unshift(newExp);
+
+        await profile.save();
+        
+        res.json(profile);
+}))
+
+
+
 export default router;
