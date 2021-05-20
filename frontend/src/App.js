18f077d19a2c0620f5dae75cf82b00d528c8c93f
khadetou <khadetou96@gmail.com>
2021-05-20 08:29:54 +0000

    login ad register ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
+import {Fragment} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import './style/App.css';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 
-const App = ()=> {
 
-  return (
-    <div >
-      <h1>Hello world</h1>
-    </div>
-  );
-}
+const App = ()=> 
+
+    <Fragment>
+      <Navbar/>
+       <Route exact path ="/" component={Landing}/>
+      <Switch>
+         <Route exact path="/login" component={Login} />
+         <Route exact path="/register" component={Register}/>
+      </Switch>
+    </Fragment>
+
+
 
 export default App;
