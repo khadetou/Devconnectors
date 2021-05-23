154ee0258d25abe4cf01e9c5e7876c4ead3fcf74
khadetou <khadetou96@gmail.com>
2021-05-23 08:44:46 +0000

    postItem config

--- frontend/src/components/posts/Post.js
+++ frontend/src/components/posts/Post.js
@@ -1,7 +1,8 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,Fragment} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {getPosts} from '../../actions/postAction';
 import Spinner from '../layout/Spinner';
+import PostItem from './/PostItem';
 const Post = () => {
     const dispatch = useDispatch();
     const {posts, loading} = useSelector(state => state.post)
@@ -10,9 +11,21 @@ const Post = () => {
         dispatch(getPosts())
     },[dispatch])
     return (
-        <div>
-            
-        </div>
+        <section className="container">
+            {loading ? <Spinner/> : 
+            (<Fragment>
+               <h1 className="text-large text-primary">Posts</h1>
+               <p className="lead">
+                   <i className="fas fa-user"></i> Welcome to the community
+               </p>
+               {/* Post form  */}
+               <div className="posts">
+                   {posts !==null && posts.map((post,idx)=>(
+                       <PostItem key={idx} post={post}/>
+                   ))}
+               </div>
+            </Fragment>)}
+        </section>
     )
 }
 
